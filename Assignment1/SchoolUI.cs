6fc4015b73a670b08f833e3daf45170c2391f398
Sotiris Ploumis <sotiris.ploumis@gmail.com>
2019-02-20 15:16:26 +0200

    moved trainer ops to trainermanager

--- Assignment1/SchoolUI.cs
+++ Assignment1/SchoolUI.cs
@@ -83,10 +83,10 @@ namespace Assignment1
 					Console.ReadKey();
 					break;
 				case MenuOptions.InputTrainers:
-					InputTrainers();
+					TrainerManager.InputTrainers();
 					break;
 				case MenuOptions.ShowTrainers:
-					ShowTrainers(true);
+					TrainerManager.ShowTrainers(true);
 					Console.ReadKey();
 					break;
 				case MenuOptions.InputAssignments:
@@ -126,135 +126,6 @@ namespace Assignment1
 			}
 
 			
-		}
-
-
-		//trainers
-
-		private static void InputTrainers()
-		{
-			string option = SchoolUI.ManualOrAuto("trainers");
-
-			if (option.Equals("a") || option.Equals("A"))
-			{
-				SchoolUI.AutoFillTrainers();
-			}
-			else
-			{
-				SchoolUI.ManualFillTrainers();
-			}
-		}
-
-		private static void ShowTrainers(bool inFull)
-		{
-			if (TrainerList.Count < 1)
-			{
-				Console.WriteLine("No trainers yet\n");
-				return;
-			}
-
-			Console.WriteLine("TRAINERS");
-
-			foreach (Trainer t in TrainerList)
-			{
-				if (inFull)
-				{
-					Console.WriteLine($"{TrainerList.IndexOf(t) + 1}: {t.FirstName} {t.LastName}, teaches {t.Subject}");
-				}
-				else
-				{
-					Console.WriteLine($"{TrainerList.IndexOf(t) + 1}: {t.FirstName} {t.LastName}");
-				}
-			}
-			Console.WriteLine();
-		}
-
-		private static void AutoFillTrainers()
-		{
-			string current = Directory.GetCurrentDirectory();
-
-			string path = Path.Combine(current, @"..\..\Data\autotrainers.txt");
-
-			string[] allTrainers = new string[1];
-			try
-			{
-				allTrainers = File.ReadAllLines(path);
-			}
-			catch (FileNotFoundException)
-			{
-				Console.WriteLine("auto trainers file not found\n");
-				return;
-			}
-
-			foreach (string line in allTrainers)
-			{
-				string[] items = line.Split('-');
-				string fname = items[0];
-				string lname = items[1];
-				string subject = items[2];
-				
-				Trainer t = new Trainer()
-				{
-
-					FirstName = fname,
-					LastName = lname,
-					Subject = subject
-				};
-
-				TrainerList.Add(t);
-
-			}
-			if (TrainerList.Count < 1)
-			{
-				Console.WriteLine("Couldn't auto save any trainers");
-			}
-			else
-			{
-				Console.WriteLine($"Successfully saved {TrainerList.Count} trainers");
-			}
-			Console.WriteLine();
-		}
-
-		private static void ManualFillTrainers()
-		{
-			Trainer t;
-			while (true)
-			{
-				Console.WriteLine("type a new trainer");
-				Console.WriteLine("firstName - lastName - Subject");
-				Console.WriteLine("to quit type 'exit' or '0' and hit Enter");
-
-				string input = Console.ReadLine();
-
-				if (input.Trim().Equals("exit") || input.Trim().Equals("0"))
-				{
-					break;
-				}
-
-				string[] items = input.Split('-');
-
-				if (items.Length < 3)
-				{
-					Console.WriteLine("An argument is missing\n");
-					continue;
-				}
-
-				
-				string fname = items[0].Trim();
-				string lname = items[1].Trim();
-				string subject = items[2].Trim();
-
-				t = new Trainer()
-				{
-					FirstName = fname,
-					LastName = lname,
-					Subject = subject
-				};
-
-				TrainerList.Add(t);
-
-				Console.WriteLine($"Trainer {t.FirstName} {t.LastName} saved\n");
-			}
 		}
 
 		//assignments
@@ -784,7 +655,7 @@ namespace Assignment1
 				return;
 			}
 
-			ShowTrainers(false);
+			TrainerManager.ShowTrainers(false);
 
 			ShowCourses(false);
 
---
+++ Assignment1/TrainerManager.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace Assignment1
+{
+	class TrainerManager
+	{
+		public static void InputTrainers()
+		{
+			string option = SchoolUI.ManualOrAuto("trainers");
+
+			if (option.Equals("a") || option.Equals("A"))
+			{
+				AutoFillTrainers();
+			}
+			else
+			{
+				ManualFillTrainers();
+			}
+		}
+
+		public static void ShowTrainers(bool inFull)
+		{
+			if (SchoolUI.TrainerList.Count < 1)
+			{
+				Console.WriteLine("No trainers yet\n");
+				return;
+			}
+
+			Console.WriteLine("TRAINERS");
+
+			foreach (Trainer t in SchoolUI.TrainerList)
+			{
+				if (inFull)
+				{
+					Console.WriteLine($"{SchoolUI.TrainerList.IndexOf(t) + 1}: {t.FirstName} {t.LastName}, teaches {t.Subject}");
+				}
+				else
+				{
+					Console.WriteLine($"{SchoolUI.TrainerList.IndexOf(t) + 1}: {t.FirstName} {t.LastName}");
+				}
+			}
+			Console.WriteLine();
+		}
+
+		private static void AutoFillTrainers()
+		{
+			string current = Directory.GetCurrentDirectory();
+
+			string path = Path.Combine(current, @"..\..\Data\autotrainers.txt");
+
+			string[] allTrainers = new string[1];
+			try
+			{
+				allTrainers = File.ReadAllLines(path);
+			}
+			catch (FileNotFoundException)
+			{
+				Console.WriteLine("auto trainers file not found\n");
+				return;
+			}
+
+			foreach (string line in allTrainers)
+			{
+				string[] items = line.Split('-');
+				string fname = items[0];
+				string lname = items[1];
+				string subject = items[2];
+
+				Trainer t = new Trainer()
+				{
+
+					FirstName = fname,
+					LastName = lname,
+					Subject = subject
+				};
+
+				SchoolUI.TrainerList.Add(t);
+
+			}
+			if (SchoolUI.TrainerList.Count < 1)
+			{
+				Console.WriteLine("Couldn't auto save any trainers");
+			}
+			else
+			{
+				Console.WriteLine($"Successfully saved {SchoolUI.TrainerList.Count} trainers");
+			}
+			Console.WriteLine();
+		}
+
+		private static void ManualFillTrainers()
+		{
+			Trainer t;
+			while (true)
+			{
+				Console.WriteLine("type a new trainer");
+				Console.WriteLine("firstName - lastName - Subject");
+				Console.WriteLine("to quit type 'exit' or '0' and hit Enter");
+
+				string input = Console.ReadLine();
+
+				if (input.Trim().Equals("exit") || input.Trim().Equals("0"))
+				{
+					break;
+				}
+
+				string[] items = input.Split('-');
+
+				if (items.Length < 3)
+				{
+					Console.WriteLine("An argument is missing\n");
+					continue;
+				}
+
+
+				string fname = items[0].Trim();
+				string lname = items[1].Trim();
+				string subject = items[2].Trim();
+
+				t = new Trainer()
+				{
+					FirstName = fname,
+					LastName = lname,
+					Subject = subject
+				};
+
+				SchoolUI.TrainerList.Add(t);
+
+				Console.WriteLine($"Trainer {t.FirstName} {t.LastName} saved\n");
+			}
+		}
+	}
+}
